637831eb86cb89ee11b0d9f44bc764b121ddc773
Dominik Modrzejewski <dominikmodrzejewski@pw.pl>
2021-09-21 17:14:21 +0200

    fixed mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 const navMobile = document.querySelector('.nav-mobile');
+const allNavLink = document.querySelectorAll('.nav__link');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+
+
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active');
 
+    allNavLink.forEach(link => {
+        link.addEventListener('click', () => {
+            navMobile.classList.remove('nav-mobile--active');
+        })
+    })
+
 
 
 }
+
+
 
 navBtn.addEventListener('click', handleNav);
 
